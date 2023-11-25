bca56810d580d27efc434384ba9712842f94cf9c
balusena <balusena12@gmail.com>
2023-11-25 21:08:55 +0000

    Redis script file.

--- redis.sh
+++ redis.sh
@@ -5,7 +5,7 @@ echo -e "\e[33mEnable Redis 6 version\e[0m"
 yum module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mInstall Redis\e[0m"
-yum install redis -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+yum install redis -y &>>/tmp/roboshop.log
 
 echo -e "\e[33mUpdate Redis Listen Address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
