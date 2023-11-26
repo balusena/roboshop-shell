d822da51a0d21172bc13f241c4b7e82af19a49ae
balusena <balusena12@gmail.com>
2023-11-26 10:22:35 +0000

    MongoDB script files.

--- mongodb.sh
+++ mongodb.sh
@@ -4,6 +4,9 @@ cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/
 echo -e "\e[33mInstalling MongoDB Server\e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
 
+echo -e "\e[33mUpdate MongoDB Listen Address\e[0m"
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
+
 echo -e "\e[33mEnable and Restart MongoDB Service\e[0m"
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod &>>/tmp/roboshop.log
